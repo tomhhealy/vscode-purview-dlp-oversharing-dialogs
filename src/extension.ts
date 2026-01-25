94c5df90fed20e349b406292dcf21ffc669fb1a0
Tom Healy <me@t-h.io>
2026-01-25 16:04:54 +0000

    Add command to create Purview DLP oversharing dialog template

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,64 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-purview-dlp-oversharing-dialogs" is now active!',
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-purview-dlp-oversharing-dialogs" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "vscode-purview-dlp-oversharing-dialogs.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from vscode-purview-dlp-oversharing-dialogs!",
+      );
+    },
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-purview-dlp-oversharing-dialogs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-purview-dlp-oversharing-dialogs!');
-	});
+  const createTemplateDisposable = vscode.commands.registerCommand(
+    "vscode-purview-dlp-oversharing-dialogs.createTemplate",
+    async () => {
+      // Create the JSON template structure
+      const template = {
+        LocalizationData: [
+          {
+            Language: "en-US",
+            Title: "",
+            Body: "",
+            Options: ["Option 1", "Option 2"],
+          },
+        ],
+        HasFreeTextOption: true,
+        DefaultLanguage: "en-US",
+      };
 
-	context.subscriptions.push(disposable);
+      // Create a new untitled document with JSON language
+      const document = await vscode.workspace.openTextDocument({
+        content: JSON.stringify(template, null, 2),
+        language: "json",
+      });
+
+      // Show the document in the editor
+      await vscode.window.showTextDocument(document);
+
+      vscode.window.showInformationMessage(
+        "Purview DLP oversharing dialog template created!",
+      );
+    },
+  );
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(createTemplateDisposable);
 }
 
 // This method is called when your extension is deactivated
